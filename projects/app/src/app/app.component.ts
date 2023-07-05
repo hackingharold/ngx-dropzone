01a5221f0c27863a1d0d7dd49f265199fde5f43d
hackingharold <hackingharold@mailbox.org>
2023-07-06 01:54:17 +0200

    refactor: rename "tests" app to "app".

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -2,24 +2,33 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: `
+  selector: 'app-root',
+  template: `<div class="app-container">
     <mat-form-field appearance="outline">
       <ngx-mat-dropzone>
-        <mat-label>Drop it!</mat-label>
-        <input type="file" fileInput formControlName="profileImg" accepts="image/jpeg" />
+        <input type="file" fileInput [formControl]="profileImg" accepts="image/jpeg" />
       </ngx-mat-dropzone>
-      <mat-error>Invalid file!</mat-error>
+      <mat-error>Only image files allowed!</mat-error>
     </mat-form-field>
 
     <div style="width: 40px"></div>
 
     <mat-form-field appearance="fill">
       <ngx-mat-dropzone>
-        <mat-label>Drop it!</mat-label>
         <input type="file" fileInput />
       </ngx-mat-dropzone>
-    </mat-form-field>`,
-  template: `app works!.`,
+    </mat-form-field>
+  </div>`,
+  styles: [
+    `
+      .app-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 25vh;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   profileImg = new FormControl();
