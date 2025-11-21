f2331111a5f39655f14c49fce098d8c99b8378ff
hackingharold <hackingharold@mailbox.org>
2025-11-21 13:08:49 +0100

    feat: update core and cli to v21

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations()],
+  providers: [provideZoneChangeDetection(), provideAnimations()],
 }).catch((e) => console.error(e));
