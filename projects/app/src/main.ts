310a4b1a92d18493d10f582b93dfe4ff6251b6c9
hackingharold <hackingharold@mailbox.org>
2025-11-21 13:16:05 +0100

    feat: update schematics to v21

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,8 +1,6 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZoneChangeDetection(), provideAnimations()],
+  providers: [],
 }).catch((e) => console.error(e));
